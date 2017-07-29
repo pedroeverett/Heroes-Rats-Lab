fa67c5ced337c44b21f07c096fa36264db43fa7e
Pedro Everett <pedrojoseeverett@gmail.com>
2017-07-29 09:16:49 +0100

    hero can talk saying his name test created

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -15,9 +15,9 @@ describe("Hero", function() {
 
   beforeEach(function() {
 
-    hero = new Hero("Red", 100, "Red Peppers", "Yummyy");
+    hero = new Hero("Red", 100, "Red Peppers", "My name is");
     food1 = new Food("Cookie", 10);
-    food2 = new Food("IceCream", 30);
+    food2 = new Food("Red Peppers", 30);
     food3 = new Food("Chicken", 5);
     task1 = new Task(1,5, "Ballon", true);
     task2 = new Task(10,1, "Puppy", false);
@@ -30,6 +30,10 @@ describe("Hero", function() {
 
   it("health starts at 100", function() {
     assert.equal(hero.health, 100);
+  }),
+
+  it("can talk saying his name", function() {
+    assert.equal(hero.sayName(), "My name is Red");
   }),
 
   it("taskList starts empty", function() {
